de0880705d954d104dba46bb55653fa3b15861f3
jonas schaerlaeken <jonas.schaerlaeken@telenet.be>
2026-02-15 19:10:34 +0100

    Fix van backend url.

--- frontend/src/app/services/PersonService.ts
+++ frontend/src/app/services/PersonService.ts
@@ -9,7 +9,7 @@ import { Person } from '../models/interfaces';
 export class PersonService {
   
   // Communicate with the real backend.
-  private apiUrl = `https://gosmartlib.tech//api/hello`;
+  private apiUrl = `https://gosmartlib.tech/api/hello`;
   // Communicate with test backend.
   private testApiUrl = 'http://localhost:8080/api/hello';
 
