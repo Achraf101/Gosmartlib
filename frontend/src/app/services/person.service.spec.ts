5746ade8b24c4a753b329cccffc0a2530a825922
BVAV <brechtvanackerstudent@gmail.com>
2026-02-16 17:23:16 +0100

    fixed import

--- frontend/src/app/services/person.service.spec.ts
+++ frontend/src/app/services/person.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { PersonService } from './person.service';
+import { PersonService } from '../services/PersonService';
 
 describe('PersonService', () => {
   let service: PersonService;
