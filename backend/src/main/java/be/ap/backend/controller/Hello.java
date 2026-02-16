694d9afb0ded9140b9bd5ec6fb5a70b07898776a
BVAV <brechtvanackerstudent@gmail.com>
2026-02-16 17:23:50 +0100

    removed unnecessary crossorigin

--- backend/src/main/java/be/ap/backend/controller/Hello.java
+++ backend/src/main/java/be/ap/backend/controller/Hello.java
@@ -1,15 +1,10 @@
 package be.ap.backend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.ap.backend.controller.dto.PersonDTO;
 
-// Accept the real frontend.
-@CrossOrigin(origins = "https://gosmartlib.tech")
-// Accept test frontend.
-// @CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
 public class Hello {
