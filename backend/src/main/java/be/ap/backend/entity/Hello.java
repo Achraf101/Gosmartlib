554184dbb942e641625377948ceea771d8329a0b
Van Acker Voorspoels Brecht (s151128) <brecht.vanackervoorspoels@student.ap.be>
2026-03-03 23:10:07 +0000

    fix Hello.java id generation

--- backend/src/main/java/be/ap/backend/entity/Hello.java
+++ backend/src/main/java/be/ap/backend/entity/Hello.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Hello {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String msg;
