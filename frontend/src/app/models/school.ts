e23d5e18372d61fc79d187555f28666c7fd31e47
Jasper Van Hove <jaspervanhove6@gmail.com>
2026-03-03 15:06:16 +0100

    add everything to add a school to frontend

---
+++ frontend/src/app/models/school.ts
@@ -0,0 +1,7 @@
+export interface School {
+  id: number;
+  name: string;
+  adres?: string;
+  contact?: string;
+  description?: string;
+}
---
+++ frontend/src/app/services/school.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api';
+import { School } from '../models/school';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SchoolService {
+  private readonly endpoint = 'school';
+
+  constructor(private apiService: ApiService) {}
+
+  addSchool(school: Omit<School, 'id'>): Observable<School> {
+    return this.apiService.post<School>(this.endpoint, school);
+  }
+}
