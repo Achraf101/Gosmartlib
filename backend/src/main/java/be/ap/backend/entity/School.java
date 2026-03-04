6d0b7d63c5c060980cc42b51f515016b4d947ae8
Jasper Van Hove <jaspervanhove6@gmail.com>
2026-03-04 15:42:34 +0100

    update school entity

--- backend/src/main/java/be/ap/backend/entity/School.java
+++ backend/src/main/java/be/ap/backend/entity/School.java
@@ -1,9 +1,13 @@
 package be.ap.backend.entity;
 
+import java.util.List;
+
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "school")
+@Data
 public class School {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +26,10 @@ public class School {
     @Column(name = "description", length = 1000)
     private String description;
 
+    // One school has many campuses
+    @OneToMany(mappedBy = "school", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Campus> campuses;
+
     public School() {
 
     }
@@ -30,46 +38,6 @@ public class School {
         this.name = name;
         this.adres = adres;
         this.contact = contact;
-        this.description = description;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAdres() {
-        return adres;
-    }
-
-    public void setAdres(String adres) {
-        this.adres = adres;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
     }
 }
