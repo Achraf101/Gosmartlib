4730008b80ac0ee08cd23d60ecb3e1c56cf4977e
BVAV <brechtvanackerstudent@gmail.com>
2026-03-06 14:07:23 +0100

    add bookform to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { BookformComponent } from './components/bookform/bookform';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'boek',
+    component: BookformComponent,
+    children: [
+      {
+        path: 'toevoegen',
+        component: BookformComponent,
+      },
+    ],
+  },
+];
