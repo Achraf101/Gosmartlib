71ffc959e7b7f01fc0c3b3aa22114fa1674f20e6
jonas schaerlaeken <jonas.schaerlaeken@telenet.be>
2026-03-06 16:50:49 +0100

    change settings for application

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BookDetailPage } from './components/book-detail-page/book-detail-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'book/:id',
+    component: BookDetailPage,
+  },
+];
