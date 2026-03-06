3db8f3f0dcc26ef6bf6b6cbcb38f1cd735412775
BVAV <brechtvanackerstudent@gmail.com>
2026-03-06 14:07:58 +0100

    update hello to generate id

--- backend/src/main/java/be/ap/backend/entity/Hello.java
+++ backend/src/main/java/be/ap/backend/entity/Hello.java
@@ -1,13 +1,12 @@
 package be.ap.backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "hello")
 public class Hello {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String msg;
