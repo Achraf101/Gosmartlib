f9a610f00c94499255cc0d752bdf9a8c4bb1e417
Achraf101 <achraf.elmsaiti@gmail.com>
2026-03-07 01:46:36 +0100

    Remove TestBook api

--- backend/src/main/java/be/ap/backend/repository/TestBookRepository.java
+++
@@ -1,8 +0,0 @@
-package be.ap.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import be.ap.backend.entity.TestBook;
-
-public interface TestBookRepository extends JpaRepository<TestBook, Long> {
-}
