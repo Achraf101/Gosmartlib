1b1c5f7c3cda07684d8f36b4608bbbfce3ac4bb3
Muhammed Gurkan <s162117@ap.be>
2026-03-08 15:46:29 +0100

    add Section entity

---
+++ backend/src/main/java/be/ap/backend/entity/Section.java
@@ -0,0 +1,27 @@
+package be.ap.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "section")
+public class Section {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(length = 255, name = "title")
+    private String title;
+
+    @Column(name = "ranking")
+    private byte ranking;
+
+    @Column(name = "school_id")
+    private Long schoolId;
+
+    @Column(name = "hidden")
+    private boolean hidden;
+}
