91cc6386a04de51e5816fa55fb22d9e1033db0c7
Achraf101 <achraf.elmsaiti@gmail.com>
2026-03-10 22:55:06 +0100

    Add search to catalogue

--- frontend/src/app/services/book.ts
+++ frontend/src/app/services/book.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api';
 import { Observable } from 'rxjs';
-import { BookDetail, BookFilter, BookResult, CreateBook } from '../models/book';
+import { BookDetail, BookResult, CreateBook } from '../models/book';
 import { Page } from '../models/page';
 import { HttpParams } from '@angular/common/http';
 
@@ -18,10 +18,12 @@ export class BookService {
     return this.apiService.get<Page<BookResult>>(this.endpoint, params);
   }
 
-  getFiltered(query: string, filters: BookFilter): Observable<BookResult[]> {
-    // transform bookfilter to http params using eg: genre=1,2,3 (spring boot supports this)
-    // params: HttpParams;
-    return this.apiService.get<BookResult[]>(`${this.endpoint}/search/${query}` /*params*/);
+  search(query: string, page: number = 0, size: number = 5): Observable<Page<BookResult>> {
+    const params = new HttpParams().set('page', page.toString()).set('size', size.toString());
+    return this.apiService.get<Page<BookResult>>(
+      `${this.endpoint}/search/${encodeURIComponent(query)}`,
+      params,
+    );
   }
 
   getById(id: number): Observable<BookDetail> {
