11d13518b85fd4c2fb2a6858062c9528766c774f
Jasper Van Hove <jaspervanhove6@gmail.com>
2026-03-13 11:09:27 +0100

    add campus book entity

---
+++ backend/src/main/java/be/ap/backend/entity/CampusBook.java
@@ -0,0 +1,32 @@
+package be.ap.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "campus_book")
+@NoArgsConstructor
+@AllArgsConstructor
+public class CampusBook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "campus_id", nullable = false)
+    private Long campusId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    private Long bookId;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Column(name = "current_amount", nullable = false)
+    private Integer currentAmount;
+
+    @Column(name = "location", length = 255)
+    private String location;
+}
