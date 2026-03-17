4b2e8d6c24fa4bc4c9af28e337488203368f4605
Jasper Van Hove <jaspervanhove6@gmail.com>
2026-03-17 11:06:01 +0100

    add and update needed services

---
+++ frontend/src/app/services/campusbook.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CampusBook } from '../models/CampusBook';
+import { ApiService } from './api';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CampusBookService {
+  private readonly endpoint = 'campusbook';
+
+  constructor(private apiService: ApiService) {}
+
+  createCampusBook(campusBook: Omit<CampusBook, 'id'>): Observable<CampusBook> {
+    return this.apiService.post<CampusBook>(this.endpoint, campusBook);
+  }
+
+  getAll(): Observable<CampusBook[]> {
+    return this.apiService.get<CampusBook[]>(this.endpoint);
+  }
+
+  getByCampus(campusId: number, page: number, rows: number): Observable<any> {
+    return this.apiService.get<any>(
+      `${this.endpoint}/campus/${campusId}?page=${page}&size=${rows}`,
+    );
+  }
+}
