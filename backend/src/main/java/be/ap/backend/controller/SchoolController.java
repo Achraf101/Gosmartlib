ca62089fe5d5572845cda9f5afe3c5e5c21ffaa1
Jasper Van Hove <jaspervanhove6@gmail.com>
2026-03-19 15:43:07 +0100

    change school backend

--- backend/src/main/java/be/ap/backend/controller/SchoolController.java
+++ backend/src/main/java/be/ap/backend/controller/SchoolController.java
@@ -2,8 +2,10 @@ package be.ap.backend.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import be.ap.backend.entity.School;
+
+import be.ap.backend.dto.SchoolDTO;
 import be.ap.backend.service.SchoolService;
 
 @RestController
@@ -16,12 +18,19 @@ public class SchoolController {
     }
 
     @PostMapping
-    public School addSchool(@RequestBody School school) {
-        return schoolService.addSchool(school);
+    public ResponseEntity<SchoolDTO> addSchool(@RequestBody SchoolDTO dto) {
+        return ResponseEntity.ok(schoolService.addSchool(dto));
     }
 
     @GetMapping
-    public List<School> getAll() {
-        return schoolService.getAll();
+    public ResponseEntity<List<SchoolDTO>> getAll() {
+        return ResponseEntity.ok(schoolService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SchoolDTO> findById(@PathVariable Long id) {
+        return schoolService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
