ce01c807a15e36930ac3e5d83964b81eb0c7b5dc
BVAV <brechtvanackerstudent@gmail.com>
2026-03-21 12:30:43 +0100

    add updatecover query

--- backend/src/main/java/be/ap/backend/repository/BookRepository.java
+++ backend/src/main/java/be/ap/backend/repository/BookRepository.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import be.ap.backend.dto.BookCardDTO;
 import be.ap.backend.entity.Book;
+import jakarta.transaction.Transactional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findBy();
@@ -26,4 +28,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
             "OR LOWER(a.name) LIKE LOWER(CONCAT('%', :query, '%')) " +
             "OR LOWER(g.name) LIKE LOWER(CONCAT('%', :query, '%'))")
     Page<Book> search(@Param("query") String query, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Book b SET b.cover = :filename WHERE b.id = :id")
+    int updateCover(@Param("id") Long id, @Param("filename") String filename);
 }
