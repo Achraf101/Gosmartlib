4fd1dec50a4f84cb819921c27a11f053092bfbff
BVAV <brechtvanackerstudent@gmail.com>
2026-03-21 12:29:16 +0100

    remove unused import and fix formatting

--- backend/src/main/java/be/ap/backend/controller/BookController.java
+++ backend/src/main/java/be/ap/backend/controller/BookController.java
@@ -1,6 +1,5 @@
 package be.ap.backend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,7 +41,6 @@ public class BookController {
     }
 
     @GetMapping("/{id}")
-
     public Book getById(@PathVariable Long id) {
         return bookRepository.findById(id).orElse(null);
     }
