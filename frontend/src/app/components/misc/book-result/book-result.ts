dd232303a3a733ab81b9f434ce00e31b01f47681
jonas schaerlaeken <jonas.schaerlaeken@telenet.be>
2026-04-20 11:51:19 +0200

    make book-result component

---
+++ frontend/src/app/components/misc/book-result/book-result.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { BookResult as BookResultType } from '../../../models/book';
+import { Tag } from 'primeng/tag';
+import { Divider } from 'primeng/divider';
+import { BookCover } from '../book-cover/book-cover';
+
+@Component({
+  selector: 'b-result',
+  imports: [Tag, Divider, BookCover],
+  templateUrl: './book-result.html',
+  styleUrl: './book-result.css',
+})
+export class BookResult {
+  @Input() book!: BookResultType;
+
+  @Output() onBookClick = new EventEmitter<BookResultType>();
+
+  handleClick() {
+    this.onBookClick.emit(this.book);
+  }
+}
