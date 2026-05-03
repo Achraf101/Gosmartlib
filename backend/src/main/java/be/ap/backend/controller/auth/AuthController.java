8d08fe751e61b828c07fbf1bcb92855184673c48
jonas schaerlaeken <jonas.schaerlaeken@telenet.be>
2026-05-03 20:33:42 +0200

    add user beheerder with BIBLIOTHEEKBEHEERDER as role and campus 1 as campusId

--- backend/src/main/java/be/ap/backend/controller/auth/AuthController.java
+++ backend/src/main/java/be/ap/backend/controller/auth/AuthController.java
@@ -16,6 +16,7 @@ public class AuthController {
 
         final String username = (String) session.getAttribute("username");
         final String role = (String) session.getAttribute("role");
-        return ResponseEntity.ok(Map.of("username", username, "role", role));
+        final Object campus = session.getAttribute("campus");
+        return ResponseEntity.ok(Map.of("username", username, "role", role, "campusId", campus != null ? campus : 0));
     }
 }
