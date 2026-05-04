af61ce4dd7b5d033d37e80503c21742d78e691e2
Gustabird <jaspervanhove6@gmail.com>
2026-05-04 12:06:44 +0200

    add theme to bookform

--- frontend/src/app/components/bookform/bookform.ts
+++ frontend/src/app/components/bookform/bookform.ts
@@ -45,6 +45,9 @@ import { SeriesService } from '../../services/series';
 import { CharCounterComponent } from '../char-counter/char-counter';
 import { NavBarComponent } from '../nav-bar/nav-bar';
 import { BulkUpload } from '../bulk-upload/bulk-upload';
+import { Theme } from '../../models/theme';
+import { ThemeService } from '../../services/theme';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-bookform',
@@ -70,6 +73,7 @@ import { BulkUpload } from '../bulk-upload/bulk-upload';
     ToastModule,
     MessageModule,
     BulkUpload,
+    JsonPipe,
   ],
   templateUrl: './bookform.html',
   styleUrl: './bookform.css',
@@ -88,7 +92,8 @@ export class BookformComponent implements OnInit {
     didactic_material: new FormControl<boolean>(false, Validators.required),
     publisher: new FormControl<number | null>(null),
     fiction: new FormControl<boolean>(true, Validators.required),
-    genres: new FormControl<number[]>([], [maxEntries(5), Validators.required]),
+    genres: new FormControl<number[] | null>([], [maxEntries(5), Validators.required]),
+    themes: new FormControl<number[] | null>([], [maxEntries(5), Validators.required]),
     description: new FormControl<string | null>(null, [
       Validators.maxLength(500),
       Validators.required,
@@ -99,6 +104,11 @@ export class BookformComponent implements OnInit {
     font_size: new FormControl<string | null>(null),
     school: new FormControl<boolean>(false, Validators.required),
   });
+  get invalidControls() {
+    return Object.entries(this.bookForm.controls)
+      .filter(([, c]) => c.invalid)
+      .map(([k]) => k);
+  }
 
   authorForm = new FormGroup({
     name: new FormControl<string>('', Validators.required),
@@ -116,6 +126,7 @@ export class BookformComponent implements OnInit {
   });
 
   genres: Genre[] | undefined;
+  themes: Theme[] | undefined;
   languages: Language[] | undefined;
   publishers: Publisher[] | undefined;
   authors: Author[] | undefined;
@@ -146,11 +157,13 @@ export class BookformComponent implements OnInit {
     private bookTypeService: BookTypeService,
     private upLoadService: UpLoadService,
     private seriesService: SeriesService,
+    private themeService: ThemeService,
     private router: Router,
   ) {}
 
   ngOnInit() {
     this.genreService.getAll().subscribe((g) => (this.genres = g));
+    this.themeService.getAll().subscribe((t) => (this.themes = t));
     this.publisherService.getAll().subscribe((p) => (this.publishers = p));
     this.authorService.getAll().subscribe((a) => (this.authors = a));
     this.languageService.getAll().subscribe((l) => (this.languages = l));
