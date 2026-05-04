d6697ca86339b7a4d5e4a3aadc23da33fb217288
MuhammedArda17 <muhammed.gurkan@student.ap.be>
2026-05-04 12:45:09 +0200

    add ChallengeRepository and UserChallengeRepository

---
+++ backend/src/main/java/be/ap/backend/repository/UserChallengeRepository.java
@@ -0,0 +1,12 @@
+package be.ap.backend.repository;
+
+import be.ap.backend.entity.UserChallenge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface UserChallengeRepository extends JpaRepository<UserChallenge, Long> {
+    List<UserChallenge> findByUserIdAndMonth(Long userId, String month);
+    boolean existsByUserIdAndMonth(Long userId, String month);
+}
