cf50185ba3f9d70f2d6dbe30b4b8723951b49607
Achraf101 <achraf.elmsaiti@gmail.com>
2026-05-11 11:56:30 +0200

    add ReviewReport entity, repo, DTOs

---
+++ backend/src/main/java/be/ap/backend/dto/ReviewReportDTO.java
@@ -0,0 +1,23 @@
+package be.ap.backend.dto;
+
+import be.ap.backend.enums.ReviewReportStatus;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class ReviewReportDTO {
+    private Long id;
+    private Long reviewId;
+    private Long reporterUserId;
+    private String note;
+    private LocalDateTime createdAt;
+    private ReviewReportStatus status;
+
+    private double reviewRating;
+    private String reviewContent;
+    private LocalDateTime reviewAdded;
+    private Long reviewUserId;
+
+    private Long bookId;
+    private String bookTitle;
+}
