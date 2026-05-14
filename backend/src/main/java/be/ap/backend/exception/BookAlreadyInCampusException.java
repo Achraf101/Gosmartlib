445c7eb02e6830cbc3087699fd17e1a2b5db25a8
Gustabird <jaspervanhove6@gmail.com>
2026-05-14 17:24:20 +0200

    change campus logic to location and school logic in every exception

--- backend/src/main/java/be/ap/backend/exception/BookAlreadyInCampusException.java
+++
@@ -1,7 +0,0 @@
-package be.ap.backend.exception;
-
-public class BookAlreadyInCampusException extends RuntimeException {
-    public BookAlreadyInCampusException(String message) {
-        super(message);
-    }
-}
