87c6791963471fcedc3475b522153ae8534090fa
Gustabird <jaspervanhove6@gmail.com>
2026-05-14 17:25:13 +0200

    change campus logic to location and school logic in every repository

--- backend/src/main/java/be/ap/backend/repository/LocationRepository.java
+++ backend/src/main/java/be/ap/backend/repository/LocationRepository.java
@@ -3,12 +3,12 @@ package be.ap.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import be.ap.backend.entity.Campus;
+import be.ap.backend.entity.Location;
 import be.ap.backend.entity.School;
 
 import java.util.List;
 
 @Repository
-public interface CampusRepository extends JpaRepository<Campus, Long> {
-    List<Campus> findBySchool(School school);
+public interface LocationRepository extends JpaRepository<Location, Long> {
+    List<Location> findBySchool(School school);
 }
