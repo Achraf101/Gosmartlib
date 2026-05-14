8ed8208af74c717957d8d92976d2573be4619afd
Gustabird <jaspervanhove6@gmail.com>
2026-05-14 17:26:12 +0200

    fix and update data seeding

--- backend/src/main/java/be/ap/backend/BackendApplication.java
+++ backend/src/main/java/be/ap/backend/BackendApplication.java
@@ -1,55 +1,13 @@
 package be.ap.backend;
 
-import be.ap.backend.repository.CampusRepository;
-import be.ap.backend.repository.SchoolRepository;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import lombok.RequiredArgsConstructor;
-
-import be.ap.backend.entity.Campus;
-import be.ap.backend.entity.School;
-import be.ap.backend.entity.User;
-import be.ap.backend.entity.UserRole;
-import be.ap.backend.repository.UserRepository;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class BackendApplication implements CommandLineRunner {
-
-    private final CampusRepository campusRepository;
-
-    private final SchoolRepository schoolRepository;
-
-    @Value("${ADMIN_PASSWORD}")
-    private String adminPassword;
-
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
+public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        if (userRepository.findByUsername("admin").isEmpty()) {
-            userRepository.save(new User("admin", passwordEncoder.encode(adminPassword), UserRole.ADMIN));
-        }
-
-        if (schoolRepository.count() == 0) {
-            School s = schoolRepository.save(new School("AP Hogeschool", "", "", "", "aphogeschool"));
-            Campus c = new Campus();
-            c.setSchool(s);
-            c.setName("Blok A");
-            c.setAdres("");
-            c.setBorrowLimit(10);
-            c.setExtendLimit(3);
-            c.setBorrowPeriod(14);
-            c.setExtendPeriod(14);
-            campusRepository.save(c);
-        }
     }
 }
