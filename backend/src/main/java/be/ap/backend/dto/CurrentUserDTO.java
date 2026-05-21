2ee2a6db0b51db688efaf4ef46c80b5939f1d354
Gustabird <jaspervanhove6@gmail.com>
2026-05-21 15:22:47 +0200

    fix currentuser fetch

--- backend/src/main/java/be/ap/backend/dto/CurrentUserDTO.java
+++ backend/src/main/java/be/ap/backend/dto/CurrentUserDTO.java
@@ -1,14 +1,21 @@
 package be.ap.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class CurrentUserDTO {
+    @JsonProperty("userId")
     private Long userId;
     private String role;
+    @JsonProperty("schoolId")
     private Long schoolId;
+    @JsonProperty("locationId")
     private Long locationId;
+    @JsonProperty("firstName")
     private String firstName;
+    @JsonProperty("lastName")
     private String lastName;
     private String email;
     private String username;
