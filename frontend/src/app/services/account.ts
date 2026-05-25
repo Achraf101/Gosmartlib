9ece2fd1f1b133605806b058996ca3ce85e6beb0
MuhammedArda17 <muhammed.gurkan@student.ap.be>
2026-05-25 13:37:24 +0200

    fix issues

--- frontend/src/app/services/account.ts
+++ frontend/src/app/services/account.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api';
-import { Account, Password } from '../models/account';
+import { Password } from '../models/account';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,10 +10,6 @@ export class AccountService {
   private readonly endpoint = 'account';
 
   constructor(private apiService: ApiService) {}
-
-  getInfo(): Observable<Account[]> {
-    return this.apiService.get<Account[]>(this.endpoint);
-  }
 
   updatePassword(body: Password): Observable<boolean> {
     return this.apiService.put<boolean>(`${this.endpoint}/password`, body);
