9ca0b701993efc12f16c0a19bcadff2329e9f76c
jonas schaerlaeken <jonas.schaerlaeken@telenet.be>
2026-05-25 16:51:42 +0200

    if admin get all locations if beheerder get all locations of your school

--- backend/src/main/java/be/ap/backend/controller/LocationController.java
+++ backend/src/main/java/be/ap/backend/controller/LocationController.java
@@ -2,14 +2,18 @@ package be.ap.backend.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import be.ap.backend.config.SessionContext;
 import be.ap.backend.dto.LocationDTO;
+import be.ap.backend.entity.UserRole;
 import be.ap.backend.service.LocationService;
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +22,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LocationController {
     private final LocationService locationService;
+    private final SessionContext sessionContext;
+
 
     @PostMapping
     public LocationDTO createLocation(@RequestBody LocationDTO dto) {
@@ -26,7 +32,16 @@ public class LocationController {
 
     @GetMapping
     public List<LocationDTO> getAll() {
-        return locationService.findAll();
+        UserRole role = sessionContext.getRole();
+        Long schoolId;
+
+        if(role == UserRole.ADMIN)
+            return locationService.findAll();
+        else if (role == UserRole.BIBLIOTHEEKBEHEERDER){
+            schoolId = sessionContext.getSchoolId();
+            return locationService.getLocationsBySchool(schoolId);
+        }
+        throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Not allowed");
     }
 
     @GetMapping("/{id}")
