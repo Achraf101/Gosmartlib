73eb5fa225997d60291abd0ab19e4b290d94a716
Gustabird <jaspervanhove6@gmail.com>
2026-05-28 13:11:02 +0200

    change icon for loancart

--- backend/src/main/java/be/ap/backend/dto/BookResultDTO.java
+++ backend/src/main/java/be/ap/backend/dto/BookResultDTO.java
@@ -18,11 +18,11 @@ public class BookResultDTO {
     private Long id;
     private String title;
     private String cover;
-    private String author_name;
-    private BookType book_type;
-    private Long series_id;
-    private String series_name;
-    private Integer series_number;
+    private String authorName;
+    private BookType bookType;
+    private Long seriesId;
+    private String seriesName;
+    private Integer seriesNumber;
     private Language language;
     private Year published;
     private String description;
@@ -32,7 +32,7 @@ public class BookResultDTO {
     private Clib clib;
     private int pages;
     private int rating;
-    private int rating_count;
+    private int ratingCount;
 
     public BookResultDTO(Long id, String title, String cover, String author_name, BookType book_type,
             Long series_id, String series_name, Integer series_number, Language language,
@@ -41,11 +41,11 @@ public class BookResultDTO {
         this.id = id;
         this.title = title;
         this.cover = cover;
-        this.author_name = author_name;
-        this.book_type = book_type;
-        this.series_id = series_id;
-        this.series_name = series_name;
-        this.series_number = series_number;
+        this.authorName = author_name;
+        this.bookType = book_type;
+        this.seriesId = series_id;
+        this.seriesName = series_name;
+        this.seriesNumber = series_number;
         this.language = language;
         this.published = published;
         this.description = description;
@@ -53,6 +53,6 @@ public class BookResultDTO {
         this.clib = clib;
         this.pages = pages;
         this.rating = rating;
-        this.rating_count = rating_count;
+        this.ratingCount = rating_count;
     }
 }
