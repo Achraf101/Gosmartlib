06fd8b4ad75a7636230d2b836e06943f993e8e3f
BVAV <brechtvanackerstudent@gmail.com>
2026-05-29 22:14:14 +0200

    add book lookup query

--- backend/src/main/java/be/ap/backend/repository/LoanBookRepository.java
+++ backend/src/main/java/be/ap/backend/repository/LoanBookRepository.java
@@ -3,11 +3,17 @@ package be.ap.backend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import be.ap.backend.entity.Book;
 import be.ap.backend.entity.LoanBook;
 
 @Repository
 public interface LoanBookRepository extends JpaRepository<LoanBook, Long> {
     List<LoanBook> findByLoanId(Long loanId);
+
+    @Query("SELECT lb.book FROM LoanBook lb WHERE lb.loan.id = :loanId")
+    List<Book> getBooksByLoanId(@Param("loanId") Long loanId);
 }
