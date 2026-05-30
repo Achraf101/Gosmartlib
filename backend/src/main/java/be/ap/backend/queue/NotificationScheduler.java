791432a8a653eac1e63b4be55b76b898bd2f0b03
BVAV <brechtvanackerstudent@gmail.com>
2026-05-30 20:26:11 +0200

    remove logs and fix time

--- backend/src/main/java/be/ap/backend/queue/NotificationScheduler.java
+++ backend/src/main/java/be/ap/backend/queue/NotificationScheduler.java
@@ -16,15 +16,12 @@ public class NotificationScheduler {
     private final TaskQueueService taskQueueService;
     private final LoanRepository loanRepository;
 
-    @Scheduled(cron = "0 16 20 * * *", zone = "Europe/Brussels") // every day at 10:00 AM
+    @Scheduled(cron = "0 0 10 * * *", zone = "Europe/Brussels") // every day at 10:00 AM
     public void scheduleDailyNotifications() {
         // fetch all non notified loans for today (loans that need to be returned
         // tomorrow)
-        System.out.println("##############################");
-        System.out.println("cron job fired");
-        List<Long> dueLoans = loanRepository.getDueLoans(LocalDate.now().plusDays(1+13));
-        System.out.println("dueLoans.size(): ");
-        System.out.println(dueLoans.size());
+        
+        List<Long> dueLoans = loanRepository.getDueLoans(LocalDate.now().plusDays(1));
         // Push them to the queue
         dueLoans.forEach((id) -> {
             taskQueueService.push(new NotificationTask(
