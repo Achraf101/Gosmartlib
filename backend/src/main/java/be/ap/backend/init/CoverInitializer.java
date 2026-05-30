6a6b8de84ccf9abcc44c15dc93da4db69a954ec0
Gustabird <jaspervanhove6@gmail.com>
2026-05-30 12:32:53 +0200

    add javadoc to varia files

--- backend/src/main/java/be/ap/backend/init/CoverInitializer.java
+++ backend/src/main/java/be/ap/backend/init/CoverInitializer.java
@@ -10,12 +10,35 @@ import org.springframework.beans.factory.annotation.Value;
 
 import java.nio.file.*;
 
+/**
+ * Initialiseert standaard cover-bestanden bij startup van de applicatie.
+ *
+ * <p>
+ * Bij het opstarten worden resources uit de classpath gekopieerd naar de
+ * geconfigureerde uploadmap indien ze nog niet bestaan.
+ * </p>
+ *
+ * <p>
+ * Deze operatie is idempotent en overschrijft bestaande bestanden niet.
+ * </p>
+ */
 @Component
 public class CoverInitializer implements CommandLineRunner {
     private static final Logger log = LoggerFactory.getLogger(CoverInitializer.class);
     @Value("${app.upload-dir}")
     private String uploadDir;
 
+    /**
+     * Kopieert default cover-bestanden van de classpath naar de upload directory.
+     *
+     * <p>
+     * Wordt automatisch uitgevoerd bij applicatiestart via
+     * {@link CommandLineRunner}.
+     * </p>
+     *
+     * @throws Exception indien het aanmaken van directories of kopiëren van
+     *                   bestanden faalt
+     */
     @Override
     public void run(String... args) throws Exception {
         Path targetDir = Paths.get(uploadDir, "cover");
@@ -24,7 +47,7 @@ public class CoverInitializer implements CommandLineRunner {
 
         Resource[] resources = new PathMatchingResourcePatternResolver()
                 .getResources("classpath:covers/*");
-        System.out.println(resources.length);
+        log.debug("Found {} cover resources", resources.length);
 
         for (Resource res : resources) {
             if (!res.exists() || res.getFilename() == null)
--- backend/src/main/java/be/ap/backend/model/OneRosterCredentials.java
+++ backend/src/main/java/be/ap/backend/model/OneRosterCredentials.java
@@ -3,6 +3,14 @@ package be.ap.backend.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Bevat de authenticatiegegevens voor OneRoster-integratie.
+ *
+ * <p>
+ * Wordt gebruikt om API-calls te authenticeren bij een externe
+ * OneRoster-provider.
+ * </p>
+ */
 @Data
 @AllArgsConstructor
 public class OneRosterCredentials {
